f8758eef998c462598adc4a58b6685eb414bc211
Arek <kontakt@ohsofresh.pl>
2026-09-03 12:38:37 +0200

    Automatyczny deploy

--- app/View/Composers/App.php
+++ app/View/Composers/App.php
@@ -6,33 +6,35 @@ use Roots\Acorn\View\Composer;
 
 class App extends Composer
 {
-	/**
-	 * List of views served by this composer.
-	 *
-	 * @var array
-	 */
-	protected static $views = [
-		'*', // działa globalnie
-	];
+    /**
+     * List of views served by this composer.
+     *
+     * @var array
+     */
+    protected static $views = [
+        '*',
+    ];
 
-	/**
-	 * Dane dostępne we wszystkich widokach Blade.
-	 */
-	public function with(): array
-	{
-		return [
-			'siteName' => $this->siteName(),
-			'logo' => get_field('logo', 'option'),
-			'logo_footer' => get_field('logo_footer', 'option'),
-			'footer_contact' => get_field('footer_contact', 'option'),
-		];
-	}
+    /**
+     * Dane dostępne we wszystkich widokach Blade.
+     */
+    public function with(): array
+    {
+        $acfReady = did_action('acf/init');
 
-	/**
-	 * Zwraca nazwę strony.
-	 */
-	public function siteName(): string
-	{
-		return get_bloginfo('name', 'display');
-	}
+        return [
+            'siteName' => $this->siteName(),
+            'logo' => $acfReady ? get_field('logo', 'option') : null,
+            'logo_footer' => $acfReady ? get_field('logo_footer', 'option') : null,
+            'footer_contact' => $acfReady ? get_field('footer_contact', 'option') : null,
+        ];
+    }
+
+    /**
+     * Zwraca nazwę strony.
+     */
+    public function siteName(): string
+    {
+        return get_bloginfo('name', 'display');
+    }
 }
